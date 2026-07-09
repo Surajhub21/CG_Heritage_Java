173056a1f376150a685981285755c0fad5fd31ae
Suraj Mondal <sm.surojmondal@gmail.com>
2026-07-09 12:08:33 +0530

    Assessment Submiut

--- GCAssessment/src/EmployeePerformanceRanking.java
+++ GCAssessment/src/EmployeePerformanceRanking.java
@@ -1,17 +1,5 @@
 import java.util.*;
 
-class Employee {
-    int id;
-    String name;
-    int score;
-
-    Employee(int id, String name, int score) {
-        this.id = id;
-        this.name = name;
-        this.score = score;
-    }
-}
-
 public class EmployeePerformanceRanking {
 
     public static void main(String[] args) {
@@ -21,20 +9,12 @@ public class EmployeePerformanceRanking {
         int N = sc.nextInt();
 
         List<Employee> employees = new ArrayList<>();
-
         for (int i = 0; i < N; i++) {
-
-            System.out.print("Enter Employee"+i+" Id: ");
             int id = sc.nextInt();
-            System.out.print("Enter Employee"+i+" name: ");
             String name = sc.next();
-            System.out.print("Enter Employee"+i+" score: ");
             int score = sc.nextInt();
-
             employees.add(new Employee(id, name, score));
-            System.out.println();
         }
-
         double total = 0;
         for (Employee e : employees) {
             total += e.score;
@@ -52,11 +32,9 @@ public class EmployeePerformanceRanking {
                 lowest = e;
             }
         }
-
         System.out.printf("Average Score: %.2f\n", average);
         System.out.println("Highest Scorer: " + highest.name + " " + highest.score);
         System.out.println("Lowest Scorer: " + lowest.name + " " + lowest.score);
-
         List<Employee> qualified = new ArrayList<>();
 
         for (Employee e : employees) {
@@ -64,7 +42,6 @@ public class EmployeePerformanceRanking {
                 qualified.add(e);
             }
         }
-
         Collections.sort(qualified, (e1, e2) -> {
 
             if (e2.score != e1.score) {
@@ -72,11 +49,22 @@ public class EmployeePerformanceRanking {
             } else {
                 return e1.name.compareTo(e2.name);
             }
-        });
 
+        });
         System.out.println("Qualified Employees:");
         for (Employee e : qualified) {
             System.out.println(e.name + " " + e.score);
         }
+    }
+}
+class Employee {
+    int id;
+    String name;
+    int score;
+
+    Employee(int id, String name, int score) {
+        this.id = id;
+        this.name = name;
+        this.score = score;
     }
 }
